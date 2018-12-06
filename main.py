0a6e5edd1c9ecc281022f993e3cb9701d7e4162d
Xiexq21 <38846319+Xiexq21@users.noreply.github.com>
2018-12-06 13:18:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.svm import LinearSVC
 data = data_process()
 print(data.df.head(5))
 
+# k-fold split data
 def kfold_split(data, kf_train, kf_test, k):        
     kf = KFold(n_splits=k)    
     for train_index, test_index in kf.split(data):       
@@ -13,14 +14,14 @@ def kfold_split(data, kf_train, kf_test, k):
         kf_train.append(train)
         kf_test.append(test) 
             
-
+# get feature X and label y from the dataset
 def feature_label_split(data):   
     X = data[['Gender','Age','Occupation','City_Category','Stay_In_Current_City_Years',
               'Marital_Status','rProduct_Category_1','Product_Category_2','Product_Category_3']]
     y = data['Purchase']      
     return (X, y)
 
-            
+# fit model and get accuracy and weights assigned to each feature            
 def predict(X_train, y_train, X_test, y_test):    
     clf = LinearSVC(C=1.0, random_state=0)
     clf.fit(X_train, y_train)   
@@ -47,3 +48,6 @@ for train, test in train_set, test_set:
     
     scores.append(score)
     weights.append(weight)
+    
+print(scores)
+print(weights)
